682d96507b8d54af492221ba4be169ab605c53a4
yanghy <15737313694@163.com>
2018-06-04 16:55:09 +0800

    springShrio创造101

--- src/main/java/yhy/controller/ControlllerGood.java
+++ src/main/java/yhy/controller/ControlllerGood.java
@@ -10,8 +10,16 @@ public class ControlllerGood {
     @RequestMapping("/test101")
     @ResponseBody
     public String test101(){
-        System.out.println("测试如何拉去分支");
-        return "创造！";
+        System.out.println("测试如何拉取分支");
+        String str  = "520";
+        if ("520".equals("520")) {
+            System.out.println("I love you !");
+            str = "I love you";
+        } else {
+            System.out.println("I hate you");
+            str = "I hate you";
+        }
+        return "创造"+str;
     }
 
 }
