10152df8c0b2ca3afa7a70164e5c936c4e31fe5a
yanghy <15737313694@163.com>
2018-08-06 15:10:35 +0800

    springShiro中添加获取excel的方法

--- src/main/java/yhy/controller/ControllerUser.java
+++ src/main/java/yhy/controller/ControllerUser.java
@@ -1,6 +1,7 @@
 package yhy.controller;
 
 import org.apache.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
@@ -14,7 +15,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import yhy.exception.ServiceException;
 import yhy.pojo.User;
 import yhy.service.UserService;
+import yhy.util.ExcelUtil;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -62,8 +72,67 @@ public class ControllerUser {
         return "loginSuccess";
     }
 
-
-
+    /**
+     * 在controller中添加excel的获取的方法
+     *  @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping("/excel")
+    @ResponseBody
+    public String testExcel(HttpServletRequest request, HttpServletResponse response) {
+        //        获取数据
+        List<User> users = userService.getUsers();
+        System.out.println("-------user-------"+users.toString());
+//        excel 标题
+        String[] titles = {"id","username","password","interest","phone","email","age","mark","sexly"};
+        SimpleDateFormat smp = new SimpleDateFormat("yyyyMMddHHmmss");
+        String format = smp.format(new Date());
+//        excel的名称
+        String fileName = "user_info-"+format+".xls";
+//        sheet 的名字
+        String sheetName = "user_info";
+        int size = users.size();
+        String[][] content = new String[size][];
+        for (int i = 0; i < size; i ++) {
+            content[i] = new String[titles.length];
+            User user = users.get(i);
+            content[i][0] = user.getId().toString();
+            content[i][1] = user.getUsername();
+            content[i][2] = user.getPassword();
+            content[i][3] = user.getInterest();
+            content[i][4] = user.getPhone();
+            content[i][5] = user.getEmail();
+            content[i][6] = user.getAge().toString();
+            content[i][7] = user.getMark();
+            content[i][8] = user.getSexly();
+        }
+        System.out.println("-------content------"+content.toString());
+        HSSFWorkbook excel = ExcelUtil.getExcel(sheetName, titles, content, null);
+//      相应到客户端
+        this.setResponseHeader(response, fileName);
+        try {
+            OutputStream os = response.getOutputStream();
+            excel.write(os);
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Create user_info success";
+    }
+    //发送相应的流
+    public void setResponseHeader(HttpServletResponse response, String fileName) {
+        try {
+            fileName = new String(fileName.getBytes(),"ISO8859-1");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        response.setContentType("application/octet-stream;charset=ISO8859-1");
+        response.setHeader("Content-Disposition", "attachment;filename="+ fileName);
+        response.addHeader("Pargam", "no-cache");
+        response.addHeader("Cache-Control", "no-cache");
+    }
 
     @RequestMapping("/info")
     @ResponseBody
---
+++ src/main/java/yhy/util/ExcelUtil.java
@@ -0,0 +1,53 @@
+package yhy.util;
+
+import org.apache.poi.hssf.usermodel.*;
+
+/**
+ * @Author: yhy
+ * @Date: 2018/8/6 14:46
+ * @Version 1.0
+ */
+public class ExcelUtil {
+
+    /**
+     * @param sheetName sheetName的文件的名字
+     * @param title 标题
+     * @param values 对应的值
+     * @param hwb 一个excel的对象
+     * @return 返回一个 HSSFWorkbook对象
+     */
+    public static HSSFWorkbook getExcel(String sheetName, String[] title, String[][] values, HSSFWorkbook hwb) {
+        System.out.println("-----"+sheetName+"---------"+title.toString()+"-----------"+values.length+"---------");
+        //创建一个HSSFWorkbook,对应一个Excel文件
+        if (hwb == null) {
+            hwb = new HSSFWorkbook();
+        }
+//        创建一个sheet对象
+        HSSFSheet hssfSheet = hwb.createSheet(sheetName);
+//        在sheet中添加第零行
+        HSSFRow hssfRow = hssfSheet.createRow(0);
+//        创建单元格，并设置表格的，并要求居中
+        HSSFCellStyle style = hwb.createCellStyle();
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+//       声明列单元格对象
+        HSSFCell hssfCell = null;
+//        创建每列的标题
+        for (int i =0;i <title.length;i++) {
+            hssfCell = hssfRow.createCell(i);
+            hssfCell.setCellValue(title[i]);
+            hssfCell.setCellStyle(style);
+        }
+//        创建每个cell内容
+        for (int i = 0; i < values.length; i++) {
+            hssfRow = hssfSheet.createRow(i + 1); //第一行标题已经占用
+            for (int j = 0; j < values[i].length; j++) {
+                HSSFCell cell = hssfRow.createCell(j);
+//                将内容放入cell中
+                cell.setCellValue(values[i][j]);
+                cell.setCellStyle(style);
+            }
+        }
+        return hwb;
+    }
+
+}
