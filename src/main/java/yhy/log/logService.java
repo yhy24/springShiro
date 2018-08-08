3e7d6834efca46c56a35c6022560152a78e8301a
yanghy <15737313694@163.com>
2018-08-08 18:31:32 +0800

    springShiro中事物的使用

--- src/main/java/yhy/log/logService.java
+++ src/main/java/yhy/log/logService.java
@@ -1,8 +1,7 @@
 package yhy.log;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,16 +9,37 @@ import org.springframework.context.annotation.Configuration;
  * @Date: 2018/8/6 14:09
  * @Version 1.0
  * 进行aspect的配置，添加@Aspect的注解
+ * <!--进行开启事物-->
+   <aop:aspectj-autoproxy/>
  */
 @Aspect //进行开启aspect的事物
 @Configuration
 public class logService {
 
-    @Before(value = "execution(* yhy.service.impl..*(..))")
+    /**
+     * 定义一个方法，用来声明切入点的表达式，一般的，改方法中不再需要写入其他的代码
+     * 后面的其他的方法直接饮用方法名来引用当前的切点表达式即可（如下方法演示:testPCut()）
+     */
+    @Pointcut("execution(* yhy.service..*(..))")
+    public void testPoint() {
+    }
+
+    /**
+     * 调用方法前进行通知的操作Before
+     * @param joinpoint
+     */
+    @Before(value = "execution(* yhy.service..*(..))")
     public void testLog(JoinPoint joinpoint) {
         String name = joinpoint.getSignature().getName();
         System.out.println("----------I am coming-----------");
         System.out.println("----------logName--------" + name);
     }
+
+    @After("testPoint()")
+    public void testPCut() {
+        System.out.println("<-------123-------->");
+    }
+
+
 
 }
--- src/main/java/yhy/service/impl/UserServiceImpl.java
+++ src/main/java/yhy/service/impl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package yhy.service.impl;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import yhy.dao.UserDao;
 import yhy.exception.ServiceException;
@@ -22,6 +21,23 @@ public class UserServiceImpl implements UserService {
         return userDao.findUser();
     }
 
+    /*事物的传播行为*/
+    /*
+    <!--配置事物：数据源的是用注解  @Transactional-->
+    <bean id="transactionManager" class="org.springframework.jdbc.datasource.DataSourceTransactionManager">
+        <property name="dataSource" ref="dataSource"/>
+    </bean>
+    <tx:annotation-driven transaction-manager="transactionManager"/>
+     *(1.已经开启事物)当前的方法调用其他的方法(其他的方法已经有事物是的开启)
+     * propagation指定事物的传播行为，既当前事物方法被另外一个事物方法调用的时候，如何使用事物
+     * REQUIRED:既使用调用方法的事物
+     * REQUIRES_NEW:表示该方法必须启动一个行的事物，被调用事物的方法在运行就应该先挂起
+     *2.isolation：指定事物的隔离级别，最常用的值为READ_COMMITTED
+     * 默认情况下spring的声明事物对所有的运行时异常进行回滚，也可以通过对应的属性进行设置
+     * 3.readOnly:指定的事物的只读属性。ture 表示这个方法只读取数据，不进行更新的操作，这样可以帮助数据库引优化事物
+     * 4.timeOut:指定强制回滚之前事物可以占用多长时间。
+     */
+//    @Transactional(propagation = Propagation.REQUIRES_NEW,isolation = Isolation.READ_COMMITTED,readOnly = false,timeout = 1)
     @Override
     public User findUserByName(String username) throws ServiceException {
         User user = userDao.findOneUser(username);
