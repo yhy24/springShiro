4908cc123a6177f08d4fc0709e1c72d63df5ad93
yanghy <15737313694@163.com>
2018-10-23 15:11:16 +0800

    删除testBO

---
+++ src/main/java/yhy/bo/Any.java
@@ -0,0 +1,19 @@
+package yhy.bo;
+
+/**
+ * @Author: yhy
+ * @Date: 2018/10/23 15:10
+ * @Version 1.0
+ */
+public class Any {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
