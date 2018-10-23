60a6b491bfeea2cbe377c3df68122d998c9fbd2f
yanghy <15737313694@163.com>
2018-10-23 15:18:22 +0800

    test1

--- src/main/java/yhy/bo/Any.java
+++ src/main/java/yhy/bo/Any.java
@@ -11,6 +11,16 @@ public class Any {
 
     private String age;
 
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getAge() {
         return age;
     }
