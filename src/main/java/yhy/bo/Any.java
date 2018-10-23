0fb71f62cc5b4f3c8586cc32af80fb034280c251
yanghy <15737313694@163.com>
2018-10-23 15:15:35 +0800

    删除testBO

--- src/main/java/yhy/bo/Any.java
+++ src/main/java/yhy/bo/Any.java
@@ -9,6 +9,16 @@ public class Any {
 
     private String name;
 
+    private String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
